dddc6facbe333392df36575e526d86069456c477
Heather Johnson-Ledlie <heather.johnson-ledlie@spatialnetworks.com>
2020-03-12 11:21:02 -0400

    moved images to folder and added about me

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import '../App.css'
-import Logo from './Logo.png'
-import main from './main.jpg'
-import photoOne from './vvv.jpg'
-import photoTwo from './homephoto2.jpg'
-import couple from './ff.jpg'
+import Logo from '../images/Logo.png'
+import main from '../images/main.jpg'
+import photoOne from '../images/vvv.jpg'
+import photoTwo from '../images/homephoto2.jpg'
+import couple from '../images/ff.jpg'
 
 function Home() {
   return (
@@ -14,12 +14,28 @@ function Home() {
       </div>
       <img className="mainPhoto" alt="Portrait" src={main} />
       <div className="bottomLinks">
-        <p className="aboutInfo">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quos
-          odit adipisci provident laudantium debitis aperiam minima, placeat
-          tempore voluptatum error ab atque consectetur iusto voluptas velit ad
-          excepturi aspernatur.
-        </p>
+        <div className="aboutInfo">
+        <h1>Hello + Welcome!</h1>
+          <p> HEY new friend! I am Bri, owner and sole photographer of Asher + Rose Photography, + I am SO
+            excited that you're here!
+            I am just a fun lovin'  coffee obsessed momma of three who took a chance with her camera and ended up
+            ABSOLUTELY LOVING IT! 
+            I am a full service photographer who serves the Tampa bay and surrounding areas.
+            I am a natural light photographer who loves the outdoors and exploring new locations and capturing 
+            the true beauty of both the 
+            clients and their surroundings with a very minimalistic style of editing.
+            I like to call my editing style bright and moody, as I love the bright sun 
+            flares but also love my images to have a bit of a moody flare.
+            I love to use goofy prompts and not so funny jokes to get those real 
+            raw reactions out of my clients, so come prepared to laugh and have a good time at your session.
+            My favorite part of what I do, is the prep of it all! Talking with my clients prior to 
+            the session, making Pinterest boards, grabbing coffee, and just really making sure that your 
+            session is EVERYTHING you imagined It would be
+            I would love to talk with you about your vision + how I can make your fairytale come true!
+            Since we are now new friends, feel free to email me anytime! photography related or not!
+            Asher.rosephoto@gmail.com
+            </p>
+        </div>
         <div className="homePhotos">
           <div className="flexPhoto">
             <img className="photoOne" alt="Newborn portrait" src={couple} />
